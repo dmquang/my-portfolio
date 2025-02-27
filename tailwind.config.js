d6a3c38c85a1227efdfcdc44893d7c954271518c
Rudyy Greyrat <quang722008@gmail.com>
2025-02-27 15:34:14 +0700

    fix tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Quét tất cả các file JSX/TSX trong thư mục src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
